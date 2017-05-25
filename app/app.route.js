fd3c8473a505abddff8f820a663b6a9ef5baef00
Illya Moskvin <imoskvin@imamuseum.org>
2017-05-24 23:24:39 -0500

    Add AuthService and login page
    This is the big one. AuthService is somewhat greedy: redirection
    might be best handled elsewhere. Like ApiService, AuthService is
    configured in app's run(): there, we can define the login route,
    publically accessible routes, and default post-login redirect.
    
    If a user tries to access a restricted page, AuthService will
    redirect them to the login page, and if their login is successful,
    it'll then redirect them back to the restricted page they were
    requesting. If a user tries to access the login page while already
    authenticated, they'll be redirected to the default redirect target.
    
    AuthService also stores the username and token in LocalStorage,
    so that logins are persisted in between page refreshes. Whether
    other session states are persisted is yet to be determined.
    
    Lastly, AuthService will add an Authorization header to all $http
    requests, including those made by ApiService. The value it sets
    matches the format expected by the Django REST Framework for
    token-based authentication.
    
    In regards to Roundware specifically, this works out rather nicely.
    We can use the default `rest_framework_views.obtain_auth_token` DRF
    view for the login endpoint, and still be quite certain that only
    staff users will be able to login via the API. django-guardian's
    AnonymousUser does not have a password set, so they cannot login.
    Likewise, anonymous users generated during the app's normal use
    will be unable to login, since their usernames and passwords are
    randomized. As long as the API routes continue to be secured
    server-side with the proper permissions, all's well!

--- app/app.route.js
+++ app/app.route.js
@@ -22,18 +22,33 @@
                 data: {
                     cssClassnames: 'rw-state-sandbox'
                 }
+            })
+            .state('authenticate', {
+                url: '/authenticate',
+                templateUrl: 'authenticate/authenticate.html',
+                controller: 'AuthenticateController',
+                controllerAs: 'vm',
+                data: {
+                    cssClassnames: 'rw-state-authenticate'
+                }
             });
 
     }
 
 
-    run.$inject = ['ApiService'];
+    run.$inject = ['ApiService', 'AuthService'];
 
-    function run( ApiService ) {
+    function run( ApiService, AuthService ) {
 
         // TODO: Load config from file?
         ApiService.init({
             url: 'http://localhost:8888/api/2/',
+        });
+
+        AuthService.init({
+            login: '/authenticate',
+            public: ['/authenticate'],
+            redirect: '/',
         });
 
     }
---
+++ app/services/auth.service.js
@@ -0,0 +1,149 @@
+// Ideas borrowed liberally from the following tutorials:
+// http://jasonwatmore.com/post/2016/04/05/angularjs-jwt-authentication-example-tutorial
+// https://medium.com/opinionated-angularjs/techniques-for-authentication-in-angularjs-applications-7bbf0346acec
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('AuthService', Service);
+
+    Service.$inject = ['$http', '$location', '$rootScope', '$localStorage', 'ApiService'];
+
+    function Service($http, $location, $rootScope, $localStorage, ApiService) {
+
+        // config is optional, these are the default values
+        var settings = {
+            login: '/authenticate',
+            public: ['/authenticate'],
+            redirect: '/',
+        };
+
+        // track last requested restricted page
+        var lastPage = settings.redirect;
+
+        // define public interface
+        return {
+            init: init,
+            check: check,
+            login: login,
+            logout: logout,
+        }
+
+        function init( config ) {
+
+            // Replace default settings with defined configs
+            if( config !== null && typeof config === 'object' ) {
+
+                Object.keys(settings).forEach( function( key ) {
+                    settings[key] = config[key] || settings[key];
+                })
+
+            }
+
+            // Keep user logged in after page refresh
+            if( check() ) {
+                $http.defaults.headers.common.Authorization = 'token ' + $localStorage.currentUser.token;
+            }
+
+            // Redirect to login page if not logged in and trying to access a restricted page
+            // Redirect to default if logged in and trying to access login page
+            $rootScope.$on('$locationChangeStart', function (event, next, current) {
+
+                var request = $location.path();
+                var isRestricted = settings.public.indexOf(request) === -1;
+
+                // If the page is restricted and user not auth'd, redirect to login page
+                if( isRestricted && !check() ) {
+
+                    lastPage = request;
+
+                    $location.path( settings.login );
+
+                    return false;
+
+                }
+
+                // If the user is auth'd, they cannot access the login page
+                if( check() && request === settings.login ) {
+
+                    $location.path( settings.redirect );
+
+                    return false;
+
+                }
+
+                // If logout happens in-between sessions, save current page?
+                if( request !== settings.login ) {
+                    lastPage = request;
+                }
+
+            });
+
+        }
+
+        // Check if a user is currently logged in
+        function check() {
+            return typeof $localStorage.currentUser !== 'undefined';
+        }
+
+        function login(username, password, callback) {
+
+            // Clear existing user
+            logout();
+
+            ApiService.post('login', { username: username, password: password })
+                .then(
+                    function( response ) {
+
+                        // login successful if there's a token in the response
+                        if (response.data.token) {
+
+                            // store username and token in local storage to keep user logged in between page refreshes
+                            $localStorage.currentUser = { username: username, token: response.data.token };
+
+                            // add jwt token to auth header for all requests made by the $http service
+                            $http.defaults.headers.common.Authorization = 'token ' + response.data.token;
+
+                            // redirect if there's a page saved
+                            if( lastPage ) {
+                                $location.path( lastPage );
+                                lastPage = settings.redirect;
+                            }
+
+                            // execute callback with true to indicate successful login
+                            callback(true);
+
+                        } else {
+
+                            // execute callback with false to indicate failed login
+                            callback(false);
+
+                        }
+
+                    }, function( response ) {
+
+                        callback(false);
+
+                    }
+                );
+        }
+
+        function logout() {
+
+            // remove user from local storage
+            if( check() ) {
+                delete $localStorage.currentUser;
+            }
+
+            // clear http auth header
+            $http.defaults.headers.common.Authorization = '';
+
+            // redirect to the login page..?
+            $location.path( settings.login );
+
+        }
+    }
+
+})();
