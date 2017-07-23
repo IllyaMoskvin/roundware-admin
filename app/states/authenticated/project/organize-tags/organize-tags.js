019806a49b46fb01dc229d77542e683e41a81df9
Illya Moskvin <imoskvin@imamuseum.org>
2017-07-23 03:16:03 -0500

    Render tags and tag relationships into trees
    Next step is to add `treeOptions` to `ui-tree` with callbacks.

--- app/states/authenticated/project/organize-tags/organize-tags.js
+++ app/states/authenticated/project/organize-tags/organize-tags.js
@@ -4,9 +4,9 @@
         .module('app')
         .controller('OrganizeTagsController', Controller);
 
-    Controller.$inject = ['TagService', 'TagCategoryService', 'TagRelationshipService'];
+    Controller.$inject = ['$scope', 'TagService', 'TagCategoryService', 'TagRelationshipService'];
 
-    function Controller(TagService, TagCategoryService, TagRelationshipService) {
+    function Controller($scope, TagService, TagCategoryService, TagRelationshipService) {
 
         var vm = this;
 
@@ -14,16 +14,72 @@
         vm.categories = null;
         vm.tags = null;
 
+        vm.tree = [];
+
+        vm.getTag = getTag;
+
         activate();
 
         return vm;
 
+
         function activate() {
 
-            vm.relationships = TagRelationshipService.list().cache;
-            vm.categories = TagCategoryService.list().cache;
-            vm.tags = TagService.list().cache;
+            $scope.$watch( 'vm.relationships', nestRelationships, true );
 
+            vm.relationships = TagRelationshipService.list().cache.clean;
+            vm.categories = TagCategoryService.list().cache.clean;
+            vm.tags = TagService.list().cache.clean;
+
+        }
+
+
+        function getTag( tag_id ) {
+
+            return TagService.find( tag_id ).clean;
+
+        }
+
+
+        function nestRelationships( items, old ) {
+
+            var nested = convert( items );
+
+            if( typeof nested !== 'undefined' ) {
+
+                vm.tree = nested.nodes;
+
+                console.log( vm.tree );
+            }
+
+        }
+
+
+        // Adapted from https://stackoverflow.com/a/31715170/1943591
+        function convert( array ){
+            var map = {};
+            for(var i = 0; i < array.length; i++) {
+                var obj = array[i];
+                if(!(obj.id in map)){
+                    map[obj.id] = obj;
+                    map[obj.id].nodes = [];
+                }
+
+                if(typeof map[obj.id].id == 'undefined'){
+                    map[obj.id].id = obj.id;
+                    map[obj.id].tag_id = obj.tag_id;
+                    map[obj.id].parent_id = obj.parent_id;
+                }
+
+                var parent = obj.parent_id || '-';
+                if(!(parent in map)){
+                    map[parent] = {};
+                    map[parent].nodes = [];
+                }
+
+                map[parent].nodes.push(map[obj.id]);
+            }
+            return map['-'];
         }
 
     }
