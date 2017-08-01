01c70d3d40268bb3f288522ae1cb4f37af07dc1f
Illya Moskvin <imoskvin@imamuseum.org>
2017-07-31 19:11:34 -0500

    Add UI Mode radio button toggle

--- app/states/authenticated/project/build-ui/build-ui.js
+++ app/states/authenticated/project/build-ui/build-ui.js
@@ -10,6 +10,21 @@
 
         var vm = this;
 
+        // TODO: Avoid hard-coding this, serverside?
+        vm.ui_modes = [
+            {
+                label: 'Speak',
+                value: 'speak',
+            },
+            {
+                label: 'Listen',
+                value: 'listen',
+            },
+        ];
+
+        // Currently active mode
+        vm.mode = vm.ui_modes[0].value;
+
         activate();
 
         return vm;
