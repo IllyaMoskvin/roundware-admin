cbc2493e83a22f7d3127a2b70f817a92e4d820bc
Illya Moskvin <imoskvin@imamuseum.org>
2017-09-12 19:27:59 -0500

    Add SpeakerService

---
+++ app/models/speaker.service.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('SpeakerService', Service);
+
+    Service.$inject = ['DataFactory'];
+
+    function Service(DataFactory) {
+
+        return new DataFactory.Collection({
+            route: 'speakers',
+        });
+
+    }
+
+})();
