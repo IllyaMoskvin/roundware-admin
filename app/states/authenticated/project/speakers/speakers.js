62a9920997d982def96e9f26d68ddbaca3767acf
Illya Moskvin <imoskvin@imamuseum.org>
2017-09-16 01:38:39 -0500

    Draw Speakers on Leaflet map
    They aren't editable at this stage, but it's a start.

--- app/states/authenticated/project/speakers/speakers.js
+++ app/states/authenticated/project/speakers/speakers.js
@@ -4,18 +4,21 @@
         .module('app')
         .controller('SpeakersController',  Controller);
 
-    Controller.$inject = ['$scope', 'leafletDrawEvents', 'SpeakerService', 'Notification'];
+    Controller.$inject = ['$scope', 'leafletData', 'leafletDrawEvents', 'SpeakerService', 'Notification'];
 
-    function Controller($scope, leafletDrawEvents, SpeakerService, Notification) {
+    function Controller($scope, leafletData, leafletDrawEvents, SpeakerService, Notification) {
 
-        // This is where all the speaker shapes are stored
-        // https://github.com/Leaflet/Leaflet/issues/2495
-        // https://github.com/Leaflet/Leaflet/issues/1455
-        var drawnItems  = new L.geoJSON();
+        // Leafet.draw cannot render multigeometries, e.g. MultiPolygons
+        // We need to decompose MultiPolygons into FeatureGroups of Polygons
+        // This is where we will store these FeatureGroups
+        var editableGroups = [];
+
+        // We will always draw all FeatureGroups, but only one will be editable at a time.
 
         var vm = this;
 
         vm.speakers = null;
+        vm.map = null;
 
         activate();
 
@@ -23,7 +26,41 @@
 
         function activate() {
 
-            vm.speakers = SpeakerService.list().cache.dirty;
+            // This is where we'll be adding our editable FeatureGroups
+            leafletData.getMap('map').then( function( map ) {
+
+                vm.map = map;
+
+            });
+
+            SpeakerService.list().promise.then( function(cache) {
+
+                vm.speakers = cache.dirty;
+
+                vm.speakers.forEach( function( speaker ) {
+
+                    // Create a FeatureGroup for each Speaker
+                    var group = new L.geoJSON();
+
+                    // Get a copy of the shapes w/ their coord order flipped
+                    var shapes = getFlippedShapes( speaker.shape.coordinates );
+
+                    // Add the shapes as Polygons to our FeatureGroup
+                    shapes.forEach( function( shape ) {
+
+                        var poly = new L.Polygon( shape ).addTo( group );
+
+                    });
+
+                    // Add the FeatureGroup to the map
+                    group.addTo( vm.map );
+
+                    // Add the FeatureGroup to our tracker
+                    editableGroups.push( group );
+
+                });
+
+            });
 
             initLeaflet();
 
@@ -63,7 +100,7 @@
                         marker: false,
                     },
                     edit: {
-                        featureGroup: drawnItems, //REQUIRED!!
+                        featureGroup: editableGroups[0], //REQUIRED!!
                         remove: false
                     }
                 }
@@ -73,7 +110,7 @@
 
             var handle = {
                 created: function(e,leafletEvent, leafletObject, model, modelName) {
-                    drawnItems.addLayer(leafletEvent.layer);
+                    editableGroups[0].addLayer(leafletEvent.layer);
                 },
                 edited: function(arg) {},
                 deleted: function(arg) {},
@@ -97,6 +134,32 @@
                     modelName = payload.modelName;
                     handle[eventName.replace('draw:','')](e,leafletEvent, leafletObject, model, modelName);
                 });
+            });
+
+        }
+
+
+        // Django returns GeoJSON? Lat & Long are flipped.
+        // https://github.com/Leaflet/Leaflet/issues/2495
+        // https://github.com/Leaflet/Leaflet/issues/1455
+        function getFlippedShapes( coordinates ) {
+
+            // Avoid changing the original arrays
+            var shapes = angular.extend( [], coordinates );
+
+            // Flip each coordinate in each shape
+            return shapes.map( function( shape ) {
+
+                return shape.map( function( points ) {
+
+                    return points.map( function( point ) {
+
+                        return point.reverse();
+
+                    });
+
+                });
+
             });
 
         }
