bcc0b3f7ff63c73b74c915605bf128391119f112
Illya Moskvin <ivmoskvin@gmail.com>
2018-03-18 21:12:20 -0500

    Assume file URLs are absolute in Assets table

--- app/states/authenticated/project/assets/assets.js
+++ app/states/authenticated/project/assets/assets.js
@@ -163,7 +163,9 @@
 
         function getFileUrl( path ) {
 
-            return ApiService.getBaseUrl( path );
+            // If path is relative: ApiService.getBaseUrl( path )
+            // https://stackoverflow.com/a/17819167/1943591
+            return path;
 
         }
 
